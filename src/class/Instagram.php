79790f2b6f8ba17c3671a445ea26b5d92a9e0135
Adrian Martinez <am@bloombees.com>
2016-06-28 11:06:56 +0200

    Updaing bdba

---
+++ src/class/Instagram.php
@@ -0,0 +1,49 @@
+<?php
+// CloudSQL Class v10
+if (!defined ("_CloudServiceReporting_CLASS_") ) {
+    define("_CloudServiceReporting_CLASS_", TRUE);
+
+    class Instagram
+    {
+        private $core;
+        var $config;
+        var $access_token=null;
+        var $user_id=null;
+        var $error = false;
+        var $errorMsg = [];
+        function __construct (Core &$core,$config)
+        {
+            $this->core = $core;
+            $this->config = $config;
+            if(isset($config['access_token'])) $this->access_token = $config['access_token'];
+            if(isset($config['user_id'])) $this->user_id = $config['user_id'];
+        }
+
+        public function getUserRecent($user_id='', $maxId = '', $minId = '', $maxTimestamp = '', $minTimestamp = '') {
+            if(!strlen($user_id)) $user_id=$this->user_id;
+            if(strlen($user_id) && strlen($this->access_token)) {
+                $s = 'https://api.instagram.com/v1/users/%s/media/recent/';
+                $params['access_token'] = $this->access_token;
+                $params['max_id'] = $maxId;
+                $params['min_id'] = $minId;
+                $params['max_timestamp'] = $maxTimestamp;
+                $params['min_timestamp'] = $minTimestamp;
+
+                $url = sprintf($s,$user_id);
+                $ret = $this->core->request->get($url,$params);
+                if(strlen($ret) && !$this->core->request->error) {
+                    return json_decode($ret,true);
+                } else {
+                    $this->addError($this->core->request->errorMsg);
+                    return null;
+                }
+            }
+        }
+
+        function addError($value)
+        {
+            $this->error = true;
+            $this->errorMsg[] = $value;
+        }
+    }
+}
