7b97e024a7c4dea6b3a3b71abb12e486f86426d6
Adrian Martinez <am@bloombees.com>
2016-12-06 11:11:58 +0100

    Google vars a recover previous version

--- src/class/Google.php
+++ src/class/Google.php
@@ -21,12 +21,9 @@ if (!defined ("_Google_CLASS_") ) {
             $this->core = $core;
             if(!is_dir($this->core->system->root_path.'/vendor/google')) {
                 $this->addError('Missing Google Client libreries. Execute from your document root: php composer.phar require google/apiclient:^2.0');
-                $this->addError('You can find composer.phar from: https://getcomposer.org/composer.phar');
-                $this->addError('You can reduce extra files and docs with: find vendor -type d -name tests  -exec rm -rf {} \;');
-                $this->addError('You can reduce extra files and docs with: find vendor -type d -name examples  -exec rm -rf {} \;');
-                $this->addError('You can reduce extra files and docs with: find vendor -type d -name doc  -exec rm -rf {} \;');
-                $this->addError('You can reduce extra files and docs with: find vendor -type f -name "*.md"  -exec rm -rf {} \;');
+                $this->addError('You can find composer.phar from: curl https://getcomposer.org/composer.phar');
             } else {
+                $client_secret = $this->core->config->get('Google_Client');
 
                 // Read id and secret based on installed credentials
                 $client_secret = $this->core->config->get('Google_Client');
@@ -39,7 +36,6 @@ if (!defined ("_Google_CLASS_") ) {
                 if(isset($client_secret['scope'])) {
                     $this->scope = $client_secret['scope'];
                 }
-                
 
                 if(!is_array($client_secret)) {
                     $this->addError('Missing Google_Client config var with the credentials from Google. Get JSON OAUTH 2.0 credentials file from: https://console.developers.google.com/apis/credentials');
@@ -56,6 +52,13 @@ if (!defined ("_Google_CLASS_") ) {
         {
             $this->error = true;
             $this->errorMsg[] = $value;
+        }
+
+        function verifyToken($token) {
+            $ret =$this->core->request->get_json_decode('https://www.googleapis.com/oauth2/v1/tokeninfo',['access_token'=>$token]);
+            if(isset($ret['error'])) return($this->addError($ret));
+            if($this->client->getClientId() != $ret['issued_to']) return($this->addError('This token has not been generated with system client_id'));
+            return true;
         }
     }
 }
