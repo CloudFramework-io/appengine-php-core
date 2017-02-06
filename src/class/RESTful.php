a33170410cd5bf52db3cacbd473c3c51b4cdbc45
Adrian Martinez <am@bloombees.com>
2017-02-06 11:24:52 +0100

    Add X-APP-TOKEN

--- src/class/RESTful.php
+++ src/class/RESTful.php
@@ -146,7 +146,7 @@ if (!defined("_RESTfull_CLASS_")) {
             if (!strlen($origin)) $origin = ((strlen($_SERVER['HTTP_ORIGIN'])) ? preg_replace('/\/$/', '', $_SERVER['HTTP_ORIGIN']) : '*');
             header("Access-Control-Allow-Origin: $origin");
             header("Access-Control-Allow-Methods: $methods");
-            header("Access-Control-Allow-Headers: Content-Type,Authorization,X-CloudFrameWork-AuthToken,X-CLOUDFRAMEWORK-SECURITY,X-DS-TOKEN,X-REST-TOKEN,X-EXTRA-INFO,X-WEB-KEY,X-SERVER-KEY,X-REST-USERNAME,X-REST-PASSWORD");
+            header("Access-Control-Allow-Headers: Content-Type,Authorization,X-CloudFrameWork-AuthToken,X-CLOUDFRAMEWORK-SECURITY,X-DS-TOKEN,X-REST-TOKEN,X-EXTRA-INFO,X-WEB-KEY,X-SERVER-KEY,X-REST-USERNAME,X-REST-PASSWORD,X-APP-TOKEN");
             header("Access-Control-Allow-Credentials: true");
             header('Access-Control-Max-Age: 1000');
 
