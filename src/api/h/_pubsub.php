53e27234ba63bab7a046117a5bc1025a57486069
am-adnbp <ApoloZeus99>
2017-04-02 10:55:46 +0200

    Adding pubsub Google Cloud service

---
+++ src/api/h/_pubsub.php
@@ -0,0 +1,22 @@
+<?php
+class API extends RESTful
+{
+    function main()
+    {
+        /** @var PubSub $pubsub */
+        $pubsub = $this->core->loadClass('PubSub');
+        if($pubsub->error) return($this->setErrorFromCodelib('system-error',$pubsub->errorMsg));
+
+        // Create a Topic
+        if(!($topic = $pubsub->getTopic('testtopic'))) return($this->setErrorFromCodelib('system-error',$pubsub->errorMsg));
+
+        // Create a Subscription over a topic
+        if(!($subscription = $pubsub->getSubscription('testsubscription','testtopic'))) return($this->setErrorFromCodelib('system-error',$pubsub->errorMsg));
+
+        // Send a message in a topic
+        //if(!($message_id = $pubsub->publish('Test message '.uniqid('pubsub'),['test'=>1],$topic))) return($this->setErrorFromCodelib('system-error',$pubsub->errorMsg));
+
+        $this->addReturnData([$topic->info(),$pubsub->getSubscriptions(),$pubsub->getSubscriptionMessages($subscription)]);
+
+    }
+}
