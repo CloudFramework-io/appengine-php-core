bee8241fd4a1a3d9acabd9f9ed80d3f10fb43a1b
CLOUDFRAMEWORK <devteam@cloudframework.io>
2019-08-28 10:43:06 +0200

    Fix values === 0 for float type

--- src/class/DataValidation.php
+++ src/class/DataValidation.php
@@ -170,7 +170,7 @@ if (!defined ("_DATAVALIDATION_CLASS_") ) {
                 case "varbinary": case "varchar": case "char": case "string": return is_string($data);
                 case "text": case "txt": return is_string($data);
                 case "integer": if(strval(intval($data))===strval($data)) $data=intval($data);return is_integer($data);
-                case "decimal": case "float": if(floatval($data)!=0 || $data==="0") $data=floatval($data);return is_float($data);
+                case "decimal": case "float": if(floatval($data)!=0 || $data==="0" || $data === 0) $data=floatval($data);return is_float($data);
                 case "bit": if(strval(intval($data))===strval($data)) $data=intval($data);return ($data==0 || $data==1);
                 case "model": return is_array($data) && !empty($data);
                 case "json": if(is_array($data)) $data = json_encode($data);return is_string($data) && is_array(json_decode($data,true));
